1021dc6419c10af7354c7dae3f99c5e13f7ea819
a22samuelfn <a22samuelfn@192.168.56.1>
2023-01-25 09:01:58 +0100

    Ahora TODO irá bien

---
+++ src/Modelo/Xogo.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+import java.util.ArrayList;
+import java.util.Iterator;
+/**
+ *
+ * @author a22samuelfn
+ */
+public class Xogo {
+    
+    int LADO_CADRADO;
+    int MAX_Y;
+    int MAX_X;
+    int numeroLinas;
+    boolean pausa;
+    ArrayList<Cadrado> pezas = new ArrayList<>();
+    Iterator<Cadrado> iter;
+    
+    private void ePosicionValida(int x, int y){
+        iter = pezas.iterator();
+        while(iter.hasNext()){
+            
+        }
+    }
+    
+}
