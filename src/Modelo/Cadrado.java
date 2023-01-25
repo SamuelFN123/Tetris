8a004f68bd934bb17a7d37dbaad1b2854a75e37a
a22samuelfn <a22samuelfn@192.168.56.1>
2023-01-25 09:33:41 +0100

    Base Cadrado

--- src/Modelo/Cadrado.java
+++ src/Modelo/Cadrado.java
@@ -3,11 +3,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Modelo;
-
+import java.awt.Color;
+import javax.swing.JLabel;
 /**
  *
  * @author a22samuelfn
  */
 public class Cadrado {
     
+    int x;
+    int y;
+    Color corRecheo;
+    JLabel Cadrado = new JLabel();
+    
+    public String getCoordenadas(){
+        return "X: "+this.x+" Y: "+this.y;
+    }
 }
