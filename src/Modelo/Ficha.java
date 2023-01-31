cae54a5594eb15b2ce12a9b181e5e0dbafe907b1
a22samuelfn <a22samuelfn@192.168.56.1>
2023-01-31 14:28:29 +0100

    Metodos Ficha abstracta

--- src/Modelo/Ficha.java
+++ src/Modelo/Ficha.java
@@ -5,6 +5,7 @@
 package Modelo;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  *
@@ -17,4 +18,34 @@ public abstract class Ficha {
     public ArrayList<Cadrado> getCadrados() {
         return cadrados;
     }
+    
+    public boolean moverDereita(){
+        Iterator<Cadrado> iter = this.cadrados.iterator();
+        while(iter.hasNext()){
+            Cadrado tmp = iter.next();
+            tmp.setX(tmp.getX()+50);
+        }
+        return true;
+    }
+    
+    public boolean moverEsquerda(){
+        Iterator<Cadrado> iter = this.cadrados.iterator();
+        while(iter.hasNext()){
+            Cadrado tmp = iter.next();
+            tmp.setX(tmp.getX()-50);
+        }
+        return true;
+    }
+
+    public boolean moverAbaixo(){
+        Iterator<Cadrado> iter = this.cadrados.iterator();
+        while(iter.hasNext()){
+            Cadrado tmp = iter.next();
+            tmp.setY(tmp.getY()+50);
+        }
+        return true;
+    }
+    
+    public abstract boolean rotar();
+
 }
