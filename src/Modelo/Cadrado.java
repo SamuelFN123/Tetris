cec32c96a644582ab121761674fa05e05e423032
a22samuelfn <a22samuelfn@192.168.56.1>
2023-01-31 13:47:08 +0100

    Setter/Getter Cadrado

--- src/Modelo/Cadrado.java
+++ src/Modelo/Cadrado.java
@@ -19,4 +19,44 @@ public class Cadrado {
     public String getCoordenadas(){
         return "X: "+this.x+" Y: "+this.y;
     }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Color getCorRecheo() {
+        return corRecheo;
+    }
+
+    public void setCorRecheo(Color corRecheo) {
+        this.corRecheo = corRecheo;
+    }
+
+    public JLabel getCadrado() {
+        return Cadrado;
+    }
+
+    public void setCadrado(JLabel Cadrado) {
+        this.Cadrado = Cadrado;
+    }
+
+    public Cadrado(int x, int y, Color corRecheo) {
+        this.x = x;
+        this.y = y;
+        this.corRecheo = corRecheo;
+    }
+    
+    
 }
