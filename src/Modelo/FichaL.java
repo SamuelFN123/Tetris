f65d3d55c238972d174389c913bf230dc00f6ffb
a22samuelfn <a22samuelfn@192.168.56.1>
2023-02-14 12:14:05 +0100

    ¿Se pueden crear todas las piezas?

--- src/Modelo/FichaL.java
+++ src/Modelo/FichaL.java
@@ -10,6 +10,20 @@ package Modelo;
  */
 public class FichaL extends Ficha{
     
+    public FichaL(){
+        for (int i = 0; i < 4; i++) {
+            this.cadrados.add(new Cadrado(java.awt.Color.red));
+        }
+        posicionInicial();
+    }
+    
+    private void posicionInicial(){
+        this.cadrados.get(0).darPosicion(100, 100);
+        this.cadrados.get(1).darPosicion(100, 120);
+        this.cadrados.get(2).darPosicion(100, 140);
+        this.cadrados.get(3).darPosicion(120, 140);
+    }
+    
     public boolean rotar(){
         return true;
     }
